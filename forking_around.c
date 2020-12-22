5b8ca547ee18bbdd24ffc577192fd38bfa3ed3dc
Rares Cristea <cristea.rares96@gmail.com>
2020-12-22 13:47:04 +0000

    Improved the code, and documented

--- forking_around.c
+++ forking_around.c
@@ -9,19 +9,6 @@
 #include <sys/wait.h>
 #include <sys/socket.h>
 
-char *get_next_start(char *str, char delim, int terminate) {
-    char *p;
-    p = strchr(str, delim);
-    if (p != NULL) {
-        if (terminate) {
-            /* only nul-terminate the string on the second pass */
-            *p = '\0';
-        }
-        p += 1;
-    }
-    return p;
-}
-
 int main(){
 
     //Get programs that will be run
@@ -37,9 +24,12 @@ int main(){
     cmdv[0] = program1;
     cmdv[1] = program2;
     
-    char *string_list;
+    //I know, i have to make this 20 dynamic, bare with me.
+    char *string_list[20];
 
     for(int program_step = 0; program_step < program_count; program_step++){
+        printf("This is the program_step at beginning : %d\n", program_step);
+        // printf("This is strings_list: %s\n", string_list);
         int pipefd[2];
         int status;
         int bytes_read;
@@ -54,25 +44,27 @@ int main(){
 
         int procces_id = fork();
         if(procces_id == 0){
-            printf("This is process_id: %d", procces_id);
+            // printf("This is process_id: %d", procces_id);
             close(pipefd[0]); /* Close unuser read end */
-            dup2(pipefd[1], STDOUT_FILENO); /* Redirect childs stdout to parent pipe */
+             /* Redirect childs stdout to parent pipe */
+            // dup2(pipefd[2], STDERR_FILENO); /* Redirect childs stderr to parent */
             /* Execute child process */
 
             /* Different behaviour based on different programs */
             if(program_step == 0){
+                // dup2(pipefd[1], STDOUT_FILENO);
                 char *arguments[] = { cmdv[0], NULL };
                 execvp(cmdv[0], arguments);
             }
             else {
-                char *arguments[] = { cmdv[1], string_list};
-                execv(cmdv[1], arguments);
+                // printf("%s%s%s\n", string_list[0], string_list[1], string_list[2]);
+                // char *arguments[] = { cmdv[1], string_list};
+                execv(cmdv[1], string_list);
             }
             
 
             /* only if exec failed */
             exit(EXIT_FAILURE);
-
         }
         close(pipefd[1]); /* Close unused write end */
 
@@ -82,9 +74,10 @@ int main(){
             printf("Child %d terminated normally, with code %d\n",
                     procces_id, WEXITSTATUS(status));
 
+        // close(pipefd[1]);
+
         char *str;
         str = malloc(1);
-
         while(bytes_read = read(pipefd[0], &buf, BUF_LEN) > 0){
             char *tempstr = malloc(strlen(buf) + strlen(str) + 1);
             strcpy(tempstr, str);
@@ -92,39 +85,56 @@ int main(){
             str = tempstr;
         }
 
+        
+
         /* Prepare the result of ls as the arguments for cat */
         if(program_step == 0) {
+            //Replace EOL with space and count spaces
+            int space_count = 0;
             for (int i = 0; i < strlen(str); i++)
             {
-                if (str[i] == '\n') str[i] = ' ';
+                if (str[i] == '\n')
+                {
+                    str[i] = ' ';
+                    space_count++;
+                }
             }
-
-            char *p;
-            size_t i;
-
-            /* count the files that will be concatenated... */
-            i = 0;
-            for (p = str; p != NULL; p = get_next_start(p, ' ', 0)) {
-                i += 1;
+            
+            int file_name_max_size = 400; 
+            //Allocate memory to string_list
+            for (int i = 0; i < space_count; i++){
+                string_list[i] = (char *) malloc(sizeof(char) * file_name_max_size);
             }
-            printf("items: %zd\n", i);
+            
+            char modifiable_str[strlen(str)];
+            strcpy(modifiable_str, str);
 
-            string_list = malloc(sizeof(char*) * i);
+            // Set string delimitators
+            char delim[] = " ";
 
-            /* populate the table */
-            i = 0;
-            for (p = str; p != NULL; p = get_next_start(p, ' ', 1)) {
-                string_list[i] = p; /* store the next item... */
-                i += 1;
+            char *ptr = strtok(modifiable_str, delim);
+
+            int i = 0;
+            while(ptr != NULL)
+            {
+                // printf("'%s'\n", ptr);
+                string_list[i] = ptr;
+                ptr = strtok(NULL, delim);
+                i = i + 1;
             }
-            string_list[i] = NULL; /* terminate the list with NULL */
+            string_list[i-1]=NULL;
+            
         }
 
-        if(program_step = program_count - 1){
+        // printf("%s", string_list);
+
+        if(program_step == program_count - 1){
             final_str = str;
         }
+        printf("This is the program_step at end : %d \n", program_step);
+
     }
-    printf("%s", final_str);
+    // printf("%s", final_str);
 
     return 0;
 }
